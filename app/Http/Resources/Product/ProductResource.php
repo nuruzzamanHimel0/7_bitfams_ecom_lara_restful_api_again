11526d8aae61df7a9f0621ae1fb9b26461641396
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-02-11 22:03:53 +0600

    ProductResource done

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,12 +15,16 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
+
         return [
             'name' => $this->name,
             'description' => $this->details,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? "Out of Stock" : $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => $this->discount != 0 ? round($this->price - ($this->price * $this->discount)/100 , 2) : $this->price,
+            'rating' => $this->reviews->count() > 0 ? round( $this->reviews->sum('star') /$this->reviews->count()) : 'No Rating yet',
+            'reviews' => route('reviews.index',['product'=>$this->id]),
         ];
     }
 }
